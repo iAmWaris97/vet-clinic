087914b856744506ea30139a26af698228d7ad9b
iAmWaris97 <haleem.waris@gmail.com>
2022-11-01 21:45:33 +0430

    Alter animals table to have species column

--- schema.sql
+++ schema.sql
@@ -5,3 +5,6 @@ CREATE DATABASE vet_clinic;
 
 -- To create a table named as "animals"
 CREATE TABLE animals ( id INT, name CHAR(255), date_of_birth DATE, escape_attempts INT, neutered BOOLEAN, weight_kg DECIMAL(5,2));
+
+-- Add species column into animals table
+ALTER TABLE animals ADD species CHAR(50);
