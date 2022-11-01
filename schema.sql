3046980ce240048d424c245d6d3b2e8fb0485eee
iAmWaris97 <haleem.waris@gmail.com>
2022-11-01 17:27:47 +0430

    Create the database and animals table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,7 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+-- To creat a database named as "vet_clinic"
+CREATE DATABASE vet_clinic;
+
+-- To create a table named as "animals"
+CREATE TABLE animals ( id INT, name CHAR(255), date_of_birth DATE, escape_attempts INT, neutered BOOLEAN, weight_kg DECIMAL(5,2));
